9634edfb6554c1fc247bf01c1151a444f28f635e
Rubyc <759337828@qq.com>
2024-05-06 18:29:53 +0800

    设置与摄像头来回切换

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import Camera from '@components/Camera.vue'
 import Setting from '@components/Setting.vue'
 import { ref } from 'vue'
-import { Setting as SettingIcon} from '@icon-park/vue-next'
+import { Setting as SettingIcon, CameraFive} from '@icon-park/vue-next'
 const page = ref('setting')
 
 </script>
@@ -10,10 +10,10 @@ const page = ref('setting')
 <template>
 
   <Suspense>
-    <main class="relative bg-red-500">
-      <section class="pointer-event">
-        <SettingIcon class="no-drag absolute left-1/2 -translate-x-1/2 mt-3 cursor-pointer" theme="outline" size="24" fill="#7ed321" />
-      </section>
+    <main class="relative">
+        <SettingIcon @click="page = 'setting'" v-if="page == 'camera'" class="no-drag z-10 absolute left-1/2 -translate-x-1/2 mt-3 cursor-pointer" theme="outline" size="24" fill="#7ed321" />
+
+        <CameraFive @click="page = 'camera'" v-if="page == 'setting'" class="no-drag z-10 absolute left-1/2 -translate-x-1/2 mt-3 cursor-pointer" theme="outline" size="24" fill="#7ed321" />
 
       <section>
         <Camera v-if="page == 'camera'" />
